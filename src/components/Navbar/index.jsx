86607805a534de4453f4d8c583887004f70e6a38
Cleidson Ramos Fias <cleidson.fias@hotmail.com>
2023-09-10 20:14:00 -0300

    first commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "/styles.css"
+import { NavLink, useNavigate } from 'react-router-dom';
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="title">
+          
+        </div>
+      </nav>
+    </>
+  )
+}
