d3b88bb52ed574ed48a9c2f4071fe5f918e8aeaa
Cleidson Ramos Fias <cleidson.fias@hotmail.com>
2023-09-11 22:20:55 -0300

    feat: Create Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import "/styles.css"
-import { NavLink, useNavigate } from 'react-router-dom';
+import React from "react";
+import "./styles.css";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  const navigate = useNavigate();
-
   return (
     <>
-      <nav className="navbar">
-        <div className="title">
-          
-        </div>
-      </nav>
+      <header className="header-container">
+        <h1 className="logo-name">Carine Lima</h1>
+        <nav className="nav-container">
+          <ul className="nav-content">
+            <li>
+              <NavLink to="/" className="nav-item">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/info" className="nav-item">
+                História
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className="nav-item">
+                Contato
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </>
-  )
+  );
 }
