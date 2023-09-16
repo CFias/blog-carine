c275d7724555fd60ca857cfa73203ca51facc986
Cleidson Ramos Fias <cleidson.fias@hotmail.com>
2023-09-16 00:53:40 -0300

    feat: Cards Posts

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -4,20 +4,7 @@ import "./styles.css";
 export default function Feed() {
   return (
     <div className="card-post-container">
-      <div className="card-post-content">
-        <span className="card-post-category">
-          Podcast
-        </span>
-        <img
-          className="card-post-img"
-          src="https://media.licdn.com/dms/image/D4D03AQFwzJ4ebw2CyA/profile-displayphoto-shrink_100_100/0/1689000908080?e=1700092800&v=beta&t=mfrLGrLmHr0hWVcXxHk1XRwt0SJBgPleHN42FFv0GC8"
-          alt=""
-        />
-        <h1 className="card-post-title">Carine Lima</h1>
-        <p className="card-post-description">
-         Mulheres em Movimento
-        </p>
-      </div>
+   
     </div>
   );
 };
