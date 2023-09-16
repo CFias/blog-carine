fb1fe9cc44beba296a3a4a5496470bf76e78f165
Cleidson Ramos Fias <cleidson.fias@hotmail.com>
2023-09-16 15:08:17 -0300

    feat: Merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
-
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <Banner />
-      </BrowserRouter>
-    </div>
-=======
-import { useState } from "react";
-=======
->>>>>>> 40f1210296fd5f39fff3b7d0b25a4435c641cab0
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+ 
 // Components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer/index";
@@ -29,7 +10,6 @@ import Banner from "./components/Banner";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Register from "./pages/Register";
 import Modal from "./components/Modal";
 import { AppProvider } from "./contexts/AppContext";
 import Categories from "./components/Categories";
@@ -56,7 +36,6 @@ function App() {
         </AppProvider>
       </BrowserRouter>
     </>
->>>>>>> d3b88bb52ed574ed48a9c2f4071fe5f918e8aeaa
   );
 };
 
