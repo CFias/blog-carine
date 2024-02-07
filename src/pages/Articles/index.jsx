bb713874f045262d34629f18c54c76a96adf7f93
Cleidson Ramos Fias <116103790+CFias@users.noreply.github.com>
2024-02-07 00:41:29 -0300

    feat: Edit mobile

---
+++ src/pages/Articles/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css";
+
+export default function Articles() {
+  return (
+    <section className="articles-container container">
+    </section>
+  );
+}
