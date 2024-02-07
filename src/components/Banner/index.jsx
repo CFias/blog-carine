952f2473bc4555e38299b093bb12c65c69f86b16
Cleidson Ramos Fias <116103790+CFias@users.noreply.github.com>
2024-02-07 17:44:32 -0300

    feat: Add posts

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,20 +1,49 @@
 import React from "react";
 import "./styles.css";
 import { NavLink } from "react-router-dom";
+import { useAuthValue } from "../../contexts/AuthContext";
+import { useAuthentication } from "../../hooks/useAuthentication";
 
 export default function Banner() {
+  const { user } = useAuthValue();
+  const { logout } = useAuthentication();
+
   return (
-    <section className="banner-container">
-      <div className="banner-txt">
-        <h2 className="txt-banner">MULHERES EM MOVIMENTO</h2>
-        <p className="banner-p">
-          Mulheres virtuosas se movimentam, conquistam seu espaço com
-          dignidade e não negociam os seus valores.
-        </p>
-        <div className="banner-btns">
-          <NavLink to="/contact" className="banner-btn btn">Redes</NavLink>
+    <>
+      <section className="banner-container">
+        <div className="mob-card">
+          {user ? (
+            <div className="user-log">
+              <span>Olá, {user.displayName}</span>
+              <NavLink className="log-user" onClick={logout}>
+                Sair
+              </NavLink>
+            </div>
+          ) : (
+            <div className="no-user">
+              <NavLink
+                to="/login
+              "
+                className="user-no"
+              >
+                Login
+              </NavLink>
+            </div>
+          )}
         </div>
-      </div>
-    </section>
+        <div className="banner-txt">
+          <h2 className="txt-banner">MULHERES EM MOVIMENTO</h2>
+          <p className="banner-p">
+            Mulheres virtuosas se movimentam, conquistam seu espaço com
+            dignidade e não negociam os seus valores.
+          </p>
+          <div className="banner-btns">
+            <NavLink to="/contact" className="banner-btn btn">
+              Redes
+            </NavLink>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
