581d2347793dd2e085dad3f643bbd3468c38fa3e
Cleidson Fias <cleidson.fias@hotmail.com>
2024-07-19 15:35:24 -0300

    feat: Edit SideBar

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,23 +1,20 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Avatar, Drawer, List, ListItem, ListItemText } from "@mui/material";
+import { Drawer, List, ListItem, ListItemText } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import {
   AdminPanelSettings,
-  AlignVerticalTopSharp,
   ArrowRight,
   ExpandLess,
   ExpandMore,
   FormatListBulletedRounded,
-  HelpRounded,
-  Home,
   HomeRounded,
   InfoRounded,
-  Inventory,
   InventoryRounded,
   LogoutRounded,
   Person2Rounded,
-  Phone,
+  PersonRounded,
+  SettingsRounded,
   Share,
 } from "@mui/icons-material";
 import "./styles.css"; // Ensure to style the side menu
@@ -41,30 +38,36 @@ const SideMenu = ({ isOpen, onClose }) => {
   return (
     <Drawer anchor="left" open={isOpen} onClose={onClose}>
       <div className="side-menu">
-        <div className="side-infos">
-          {user ? (
-            <>
-              <h4>
-                <span className="icon-banner">
-                  <Person2Rounded className="icon-side" />
-                </span>{" "}
-                Olá, {user.displayName ? user.displayName : "Usuário"}
-              </h4>
-              <NavLink onClick={handleLogout} className="link-list">
-                Sair <LogoutRounded fontSize="10" className="icon-list" />
-              </NavLink>
-            </>
-          ) : (
-            <NavLink to="/login" className="link-list">
-              <p>Faça login</p>
-              <ArrowRight />
-            </NavLink>
-          )}
-        </div>
         <List>
+          <div className="side-infos">
+            {user ? (
+              <>
+                <div className="infos-pri">
+                  <span className="icon-banner">
+                    <Person2Rounded fontSize="large" className="icon-side" />
+                  </span>{" "}
+                  <div className="name-email">
+                    <h4>{user.displayName ? user.displayName : "Usuário"}</h4>
+                    <p>
+                      {user.email ? user.email : <p>Mulheres em movimento</p>}
+                    </p>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <NavLink to="/login" className="link-list">
+                <p>Faça login</p>
+                <ArrowRight />
+              </NavLink>
+            )}
+          </div>
           <ListItem button component={NavLink} to="/" onClick={onClose}>
             <HomeRounded fontSize="small" className="icones" />
             <ListItemText className="side-text" primary="Inicio" />
+          </ListItem>
+          <ListItem button component={NavLink} to="/" onClick={onClose}>
+            <PersonRounded fontSize="small" className="icones" />
+            <ListItemText className="side-text" primary="Meu perfil" />
           </ListItem>
           <ListItem
             button
@@ -131,16 +134,31 @@ const SideMenu = ({ isOpen, onClose }) => {
             <InfoRounded fontSize="small" className="icones" />
             <ListItemText className="side-text" primary="Carine Lima" />
           </ListItem>
-          {user && (
-            <ListItem button component={NavLink} to="/adm" onClick={onClose}>
-              <AdminPanelSettings fontSize="small" className="icon-list" />
-              <ListItemText
-                className="side-text"
-                primary="Área administrativa"
-              />
-            </ListItem>
-          )}
+          <ListItem button component={NavLink} to="/about" onClick={onClose}>
+            <SettingsRounded fontSize="small" className="icones" />
+            <ListItemText className="side-text" primary="Configurações" />
+          </ListItem>
+          {user &&
+            (user.uid === "Ivhz4g1swbds6NZyLa6yUsGynPB3" ||
+              user.uid === "9t1GnsVos7Wem9liOT8oRU8MbU53") && (
+              <ListItem button component={NavLink} to="/adm" onClick={onClose}>
+                <AdminPanelSettings fontSize="small" className="icones" />
+                <ListItemText
+                  className="side-text"
+                  primary="Área administrativa"
+                />
+              </ListItem>
+            )}
         </List>
+        <div className="side-infos-two">
+          {user ? (
+            <NavLink onClick={handleLogout} className="link-list">
+              Sair <LogoutRounded fontSize="small" className="icones" />
+            </NavLink>
+          ) : (
+            <div className="">Caren Blog</div>
+          )}
+        </div>
       </div>
     </Drawer>
   );
